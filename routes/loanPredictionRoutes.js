8a792dd19ffc92cdb6072f3035be100165ae9232
vikram-42-alla <allatrivikram@gmail.com>
2026-02-09 12:07:09 +0530

    remove unused FEATURE_ORDER array from loanPredictionRoutes.js

--- routes/loanPredictionRoutes.js
+++ routes/loanPredictionRoutes.js
@@ -3,21 +3,7 @@ const express=require("express")
 const router=express.Router()
 
 const buildFeatures = require("../util/featureVector")
-FEATURE_ORDER = [
-    "credit_history",
-    "applicant_income_log",
-    "loan_amount_log",
-    "loan_term_log",
-    "gender_male",
-    "married_yes",
-    "dependents_1",
-    "dependents_2",
-    "dependents_3",
-    "property_semiurban",
-    "education_not_graduate",
-    "self_employed_yes",
-    "property_urban"
-]
+
 router.post("/predict",async(req,res)=>{
     try {
         console.log(req.body)
