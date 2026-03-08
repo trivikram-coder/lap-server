746acdac96a5beca77e2af981ac06a385d686bb6
vikram-42-alla <allatrivikram@gmail.com>
2026-03-08 08:18:53 +0530

    feat: implement loan application controller and update routes

---
+++ controllers/lap.controller.js
@@ -0,0 +1,75 @@
+const { default: axios } = require("axios")
+const buildFeatures = require("../util/featureVector");
+const LoanApplication = require("../model/Application");
+const User = require("../model/User");
+require("dotenv").config();
+const mlTest=async(req,res)=>{
+    try {
+        const data=await axios.get(`${process.env.ML_SERVICE_URL}`);
+        res.json(data.data)
+    } catch (error) {
+        res.json(error.message)
+    }
+}
+const predictLoan=async(req,res)=>{
+    try {
+      const features=req.body;
+      const encodedFeatures=buildFeatures(features);
+
+        const response=await axios.post(`${process.env.ML_SERVICE_URL}/predict`,
+            {
+                features:encodedFeatures
+            }
+        )
+        if(response.status!==200){
+            res.status(response.status).json(response.data);
+        }
+        await LoanApplication.create({
+            ...features,
+            status:response.data.status,
+            reasons:response.data.reasons
+        })
+        res.status(200).json(response.data)
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+}
+const fetchApplications=async(req,res)=>{
+    try {
+        const id=req.params.id;
+        const existsUser=await User.findById(id);
+        const applications=await LoanApplication.find({userId:id}).sort({createdAt:-1});
+        if(!existsUser){
+            return res.status(404).json({message:"User not exists,access denied"})
+        }
+        res.status(200).json({message:"Applications fetched successfully",data:applications})
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+}
+const getApplicationById=async(req,res)=>{
+    try{
+        const id=req.params.id;
+        const application=await LoanApplication.findById(id);
+        if(!application){
+            return res.status(404).json({message:"No application found"})
+        }
+        res.status(200).json({message:"Application fetched successfully",data:application})
+    }catch(error){
+        res.status(500).json({error:error.message})
+    }
+}
+const deleteApplicationById=async(req,res)=>{
+    try {
+        const id=req.params.id;
+        const deleteApplication=await LoanApplication.findByIdAndDelete(id);
+        if(!deleteApplication){
+            return res.status(400).json({message:"Unable to delete application"})
+
+        }
+        res.status(200).json({message:"Application deleted successfully"})
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+}
+module.exports= {mlTest,predictLoan,fetchApplications,getApplicationById,deleteApplicationById}
--- routes/lap.routes.js
+++ routes/lap.routes.js
@@ -1,49 +1,12 @@
-const { default: axios } = require("axios")
-const express=require("express")
+const express=require("express");
+const { mlTest, predictLoan, getApplicationById, fetchApplications, deleteApplicationById } = require("../controllers/lap.controller");
 const router=express.Router()
-require("dotenv").config();
-const buildFeatures = require("../util/featureVector");
-const LoanApplication = require("../model/Application");
-const User = require("../model/User");
 
-router.get("/ml-test",async(req,res)=>{
-    try {
-        const data=await axios.get(`${process.env.ML_SERVICE_URL}`);
-        res.json(data.data)
-    } catch (error) {
-        res.json(error.message)
-    }
-})
 
-router.post("/",async(req,res)=>{
-    try {
-      const features=req.body;
-      const encodedFeatures=buildFeatures(features);
+router.get("/ml-test",mlTest)
 
-        const response=await axios.post(`${process.env.ML_SERVICE_URL}/predict`,
-            {
-                features:encodedFeatures
-            }
-        )
-        if(response.status===200){
-            await LoanApplication.create(features);
-        }
-        res.status(200).json(response.data)
-    } catch (error) {
-        res.status(500).json({error:error.message})
-    }
-})
-router.get("/applications/:id",async(req,res)=>{
-    try {
-        const id=req.params.id;
-        const existsUser=await User.findById(id);
-        const applications=await LoanApplication.find({userId:id});
-        if(!existsUser){
-            return res.status(404).json({message:"User not exists,access denied"})
-        }
-        res.status(200).json({message:"Applications fetched successfully",data:applications})
-    } catch (error) {
-        res.status(500).json({error:error.message})
-    }
-})
+router.post("/",predictLoan)
+router.get("/applications/:id",fetchApplications)
+router.get("/application/:id",getApplicationById)
+router.delete("/application/:id",deleteApplicationById)
 module.exports=router
